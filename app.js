79f06e800151b3dddc216813609820039119364d
masumbillah360 <masumbillah6584@gmail.com>
2022-09-03 08:55:34 +0600

    set categories dynamically

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("connected");
+const categoriesLoader = async(url)=>{
+        const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
+        const data = await res.json();
+        setCategories(data.data.news_category);
+    
+}
+const setCategories = (categories)=>{
+    const categoryContainer = document.getElementById('category-container');
+    categories.forEach(category=>{
+        // console.log(category)
+        const categoryList = document.createElement('li');
+        categoryList.classList.add('nav-item')
+        categoryList.innerHTML  = `
+            <a onclick = "loadNews('${category.category_id}')" class="nav-link text-dark" href="#">${category.category_name}</a>
+        `
+        categoryContainer.appendChild(categoryList);
+    })
+}
+const loadNews=async(id)=>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${id}`);
+    const data = await res.json();
+    console.log(data);
+}
+categoriesLoader()
