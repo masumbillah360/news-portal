2079d2a3ea4ce9460c659f36b70c3770fb8d299a
masumbillah360 <masumbillah6584@gmail.com>
2022-09-03 17:56:58 +0600

    optimize code and bug fixed

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const categoriesLoader = async(url)=>{
         setCategories(data.data.news_category);
     } catch (error) {
         alert("Error is herer \n"+error)
-    }
-    
+    }    
 }
+
 const setCategories = (categories)=>{
     const categoryContainer = document.getElementById('category-container');
     categories.forEach(category=>{
@@ -16,7 +16,7 @@ const setCategories = (categories)=>{
         categoryList.classList.add('nav-item')
         // onclick = "loadNews('${category.category_id}')"  
         categoryList.innerHTML  = `
-            <a class="cat-btn nav-link text-dark" onmouseover = "loadNews('${category.category_id}')" href="#">${category.category_name}</a>
+            <a class="cat-btn nav-link text-dark" onclick = "loadNews('${category.category_id}','${category.category_name}')" href="#">${category.category_name}</a>
         `
         categoryContainer.appendChild(categoryList);
 
@@ -26,28 +26,24 @@ const setCategories = (categories)=>{
 categoriesLoader()
 // setCategories()
 
-const loadNews=async(id)=>{
+const loadNews = async(id,name)=>{
     const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${id}`);
     const data = await res.json();
-    const catBtn = document.getElementsByClassName('cat-btn');
-    const catCounter = document.getElementById('category-counter');
-    for(const btn of catBtn){
-        btn.addEventListener("click",(e)=>{
-            // console.log(btn);
-            // console.log(e.target.innerText);
-            catCounter.innerHTML = `
-    <div class="alert alert-secondary" role="alert">
-    <h6 class = " p-3 rounded bg-white">${data.data.length} Item's found for this ${e.target.innerText}</h6>
-    </div>
-    `;
+    // console.log(data)
+    // console.log(name)
 
+    const catCounter = document.getElementById('category-counter');
+    catCounter.innerHTML = `
+        <div class="alert alert-secondary" role="alert">
+        <h6 class = " p-3 rounded bg-white">${data.data.length} Item's found for this ${name}</h6>
+        </div>
+    `
     const newsContainer = document.getElementById('news-container');
-    
     newsContainer.textContent = '';
     const newsData =  data.data;
     newsData.forEach(news=>{
         // console.log(news)
-        const {title,rating, thumbnail_url,details,author,total_view} = news;
+        const {title,rating, thumbnail_url,details,author,total_view,_id} = news;
         const {name,published_date,img} = author;
         // console.log(published_date.length);
         const createNews = document.createElement('article');
@@ -61,7 +57,7 @@ const loadNews=async(id)=>{
                 <div class="card-body">
                     <h5 class="card-title">${title?title:"not Found"}</h5>
                     <p class="card-text pt-5">${details.length>450?details.slice(0,450)+"...":details}</p>
-                    <div class=" card-footer mt-5 row row-cols-2 row-cols-lg-4  gx-2 gx-lg-3 align-items-center text-center">
+                    <div class=" mt-5 row row-cols-2 row-cols-lg-4  gx-2 gx-lg-3 align-items-center text-center">
                         <div class="col">
                             <div class="d-flex align-items-center">
                                 <a class="navbar-brand" href="#">
@@ -85,7 +81,7 @@ const loadNews=async(id)=>{
                         <i class="fa-solid fa-star-half-stroke text-warning"></i>
                         </div>
                         <div class="col">
-                            <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop" >See More <i class="fa-solid fa-arrow-right"></i></button>
+                            <button type="button" onclick="modalNews('${_id}')" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop" >See More <i class="fa-solid fa-arrow-right"></i></button>
                         </div>
                     </div>
                 </div>
@@ -93,17 +89,38 @@ const loadNews=async(id)=>{
     
         `
         newsContainer.appendChild(createNews)
-        
     })
+}
 
-
-        })
+const modalNews = async(id)=>{
+    try {
+        const res = await fetch(` https://openapi.programming-hero.com/api/news/${id}`)
+        const data = await res.json();
+        setModalNews(data);
+    } catch (error) {
+        alert("Connect Your network"+error);
     }
 }
-// loadNews();
-loadNews('08')
-{/* <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-<i class="fa-solid fa-star-half-stroke"></i>*/}
+
+const setModalNews =(newsModal)=>{
+    // console.log(newsModal.data);
+    newsModal.data.forEach(news=>{
+        const {title, details, image_url,thumbnail_url,total_view,rating,author}= news;
+
+        const modalTitle = document.getElementById('modal-title');
+        modalTitle.innerText = title;
+        const modalBody = document.getElementById('modal-body');
+        modalBody.innerHTML = `
+        
+        <div class="card">
+            <img src="${image_url}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">${details}</p>
+            </div>
+        </div>
+
+    `
+    })
+}
+modalNews()
+// loadNews()
